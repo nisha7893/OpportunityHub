37020dbef86ab982a4fad9168b937f6ece5ccc7d
nisha7893 <nisha24.set@modyuniversity.ac.in>
2026-06-30 20:18:31 +0530

    Fix model import case

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
